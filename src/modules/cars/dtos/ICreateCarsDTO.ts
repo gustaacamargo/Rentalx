52763a5637a94b48913430a482e78ca9afc29918
Gustavo Camargo <camargo25.gustavo@gmail.com>
2021-11-10 09:06:54 -0300

    Inicia TDD com casos de uso do carro

---
+++ src/modules/cars/dtos/ICreateCarsDTO.ts
@@ -0,0 +1,11 @@
+interface ICreateCarDTO {
+  name: string;
+  description: string;
+  daily_rate: string;
+  license_plate: string;
+  fine_amount: string;
+  brand: string;
+  category_id: string;
+}
+
+export { ICreateCarDTO };
