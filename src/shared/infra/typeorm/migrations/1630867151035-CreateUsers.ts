cb01f33cad86e780ec2308f0a8b03849b728fd4b
Gustavo de Camargo <camargo25.gustavo@gmail.com>
2022-01-24 22:25:07 -0300

    Adiciona migration de aluguel

--- src/shared/infra/typeorm/migrations/1630867151035-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1630867151035-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1630867151035 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
