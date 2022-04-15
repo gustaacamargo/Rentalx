ddebb6464894b747a248dca82a492e50ea24fd7e
Gustavo de Camargo <camargo25.gustavo@gmail.com>
2022-04-14 21:45:04 -0300

    Adiciona teste no controller de criar categorias

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,5 +3,10 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 export default async (): Promise<Connection> => {
   const defautOptions = await getConnectionOptions();
 
-  return createConnection(defautOptions);
+  return createConnection(
+    Object.assign(defautOptions, {
+      database:
+        process.env.NODE_ENV === "test" ? "rentx_test" : defautOptions.database,
+    })
+  );
 };
