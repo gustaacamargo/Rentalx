9de2c437e5ed1a06fbcbb39c5211d9af194432ad
Gustavo Camargo <camargo25.gustavo@gmail.com>
2022-05-02 22:17:18 -0300

    test: corrige teste de carros

--- src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { AppError } from "@shared/errors/AppErrors";
 
@@ -8,6 +9,7 @@ import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 let dayJsProvider: DayjsDateProvider;
 
 describe("Create Rental", () => {
@@ -18,7 +20,8 @@ describe("Create Rental", () => {
     dayJsProvider = new DayjsDateProvider();
     createRentalUseCase = new CreateRentalUseCase(
       rentalsRepositoryInMemory,
-      dayJsProvider
+      dayJsProvider,
+      carsRepositoryInMemory
     );
   });
 
